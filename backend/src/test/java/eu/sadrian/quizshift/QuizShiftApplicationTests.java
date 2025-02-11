44f1a5309f73809a7fb45fd2807d9fa50da4457b
Sebastian <mail@sadrian.eu>
2025-02-11 16:08:21 +0100

    first commit

---
+++ backend/src/test/java/eu/sadrian/quizshift/QuizShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.sadrian.quizshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizShiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
