20a8728e85878e3bfa421cd701dc6d682b4a2e76
AsiXh <sebastian.adrian@gmx.de>
2025-02-12 12:19:52 +0100

    add api.js

--- backend/src/main/java/eu/sadrian/quizshift/service/AuthService.java
+++ backend/src/main/java/eu/sadrian/quizshift/service/AuthService.java
@@ -3,7 +3,9 @@ package eu.sadrian.quizshift.service;
 import eu.sadrian.quizshift.dto.LoginDTO;
 import eu.sadrian.quizshift.dto.RegisterDTO;
 import eu.sadrian.quizshift.model.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthService {
 
     public User registerUser(RegisterDTO registerDTO) {
