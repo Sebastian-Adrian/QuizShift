ef056d80f377fef9bcbab38a42b17e770a2383a2
AsiXh <sebastian.adrian@gmx.de>
2025-02-14 12:20:31 +0100

    change Cors Settings

--- backend/src/main/java/eu/sadrian/quizshift/configuration/WebConfig.java
+++ backend/src/main/java/eu/sadrian/quizshift/configuration/WebConfig.java
@@ -2,18 +2,22 @@ package eu.sadrian.quizshift.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
 
+    /*
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
@@ -21,4 +25,20 @@ public class WebConfig {
             }
         };
     }
+
+     */
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults()) // Aktiviert die CORS-Konfiguration
+
+                .csrf((csrf) -> csrf.disable()) // Deaktiviert CSRF, falls nicht benötigt
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                // TODO ersetze permitAll mit .hasRole('User)
+                                .requestMatchers("/api/auth**").permitAll().anyRequest().authenticated());
+        return http.build();
+    }
+
 }
