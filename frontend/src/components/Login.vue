52b24e4f843759d48ec3f27b918248e763a1a534
Sebastian <mail@sadrian.eu>
2025-02-15 14:54:36 +0100

    fix(login): fix missing import Formm

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,6 +3,9 @@
 import axios from "axios";
 import api from "@/api/api.js";
 import {InputText} from "primevue";
+import {zodResolver} from '@primevue/forms/resolvers/zod';
+import {z} from 'zod';
+import {Form} from "@primevue/forms";
 
 export default {
 name: "Login",
@@ -11,9 +14,25 @@ data() {
       username: "",
       password: "",
       errorMessage: "",
+      resolver: zodResolver(z.object({
+        username: z.string().min(3, { message: "Username muss mindestens 3 Zeichen lang sein" }),
+        password: z.string().min(3, { message: "Passwort muss mindestens 3 Zeichen lang sein" }),
+      }))
     };
 },
 methods: {
+
+  onFormSubmit(valid) {
+    if (valid) {
+      this.$toast.add({
+        severity: "success",
+        summary: "Success Message",
+        detail: "Anmeldung erfolgreich",
+        life: 3000
+      });
+    }
+  },
+
     async login() {
       try {
           // API-Aufruf zur Anmeldung
@@ -39,101 +58,22 @@ methods: {
 },
 };
 </script>
+
 <template>
-
-  <div class="login-container">
-    <Card header="Login">
-      <Form
-        v-slot="{ values, errors, handleSubmit, reset }"
-        :onSubmit.prevent="handleSubmit(login)"
-        class="p-fluid"
-      >
-        <div class="field">
-          <label for="username">Benutzername</label>
-          <InputText
-            id="username"
-            v-model="values.username"
-            placeholder="Benutzername eingeben"
-            required
-          />
-          <Message
-            v-if="errors.username"
-            :text="errors.username"
-            severity="error"
-          />
-        </div>
-
-        <div class="field">
-          <label for="password">Passwort</label>
-          <Password
-            id="password"
-            v-model="values.password"
-            feedback
-            placeholder="Passwort eingeben" />
-          <Message
-            v-if="errors.password"
-            :text="errors.password"
-            severity="error"
-          />
-        </div>
-
-        <Button class="mt-3" label="Anmelden" type="submit" />
-      </Form>
-
-      <p v-if="errorMessage" class="error-message mt-3">
-        {{ errorMessage }}
-      </p>
-    </Card>
-  </div>
-
-  <Form v-slot="$form" :initialValues :resolver class="flex flex-col gap-4 w-full sm:w-56" @submit="onFormSubmit">
-    <div class="flex flex-col gap-1">
-      <InputText fluid name="username" placeholder="Username" type="text" />
-      <Message v-if="$form.username?.invalid" severity="error" size="small" variant="simple">{{ $form.username.error?.message }}</Message>
-    </div>
-    <Button label="Submit" severity="secondary" type="submit" />
-  </Form>
-
-
-  <div class="login-container">
-    <h2>Login</h2>
-    <br>
-    <form @submit.prevent="login">
-      <div class="form-group">
-<!--        <label for="username">Benutzername:</label>
-        <input
-          type="text"
-          id="username"
-          v-model="username"
-          required
-          placeholder="Benutzernamen eingeben"
-        />-->
-        <FloatLabel variant="on">
-          <InputText id="username" v-model="username" required type="text">
-          </InputText>
-          <label for="username">Username</label>
-        </FloatLabel>
-      </div>
-      <br>
-      <div class="form-group">
-<!--        <label for="password">Passwort:</label>
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          required
-          placeholder="Passwort eingeben"
-        />-->
-        <FloatLabel variant="on">
-          <InputText id="password" v-model="password" required type="password">
-          </InputText>
-          <label for="password">Passwort</label>
-        </FloatLabel>
-      </div>
-
-      <button type="submit">Anmelden</button>
-    </form>
-
-    <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+  <div class="card flex justify-center">
+    <Toast />
+    <Form :resolver class="flex flex-col gap-4 w-full sm:w-56" @submit="onFormSubmit">
+      <FormField v-slot="$field" as="section" class="flex flex-col gap-2" initialValue="" name="username">
+        <InputText placeholder="Username" type="text" />
+        <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</Message>
+      </FormField>
+      <FormField v-slot="$field" asChild initialValue="" name="password">
+        <section class="flex flex-col gap-2">
+          <Password :feedback="false" fluid placeholder="Password" toggleMask type="text" />
+          <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</Message>
+        </section>
+      </FormField>
+      <Button label="Submit" severity="secondary" type="submit" />
+    </Form>
   </div>
 </template>
