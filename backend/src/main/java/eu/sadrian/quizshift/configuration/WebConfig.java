59d62f97f4e8e88e15e7e779f05a2e5333875186
Sebastian <mail@sadrian.eu>
2025-02-15 01:39:17 +0100

    feat(frontend): implement PrimeVue components in Login UI
    Replaced basic form elements in the Login component with PrimeVue components for improved user experience and consistency. Integrated new dependencies such as `@primevue/forms` and auto-import resolver in Vite configuration. Adjusted CORS setup in the backend and updated package-lock to accommodate the changes.

--- backend/src/main/java/eu/sadrian/quizshift/configuration/WebConfig.java
+++ backend/src/main/java/eu/sadrian/quizshift/configuration/WebConfig.java
@@ -1,38 +1,17 @@
 package eu.sadrian.quizshift.configuration;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfig {
-
-    /*
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-
-     */
 
     private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
