f24be226eeadb51f1bd6ed3e18c71ef9e82c5bea
Sebastian@Thinkpad <mail@sadrian.eu>
2025-02-18 10:59:13 +0100

    Add Achievements entity to the data model
    Introduced the Achievements class as a JPA entity to represent achievements in the database.

---
+++ backend/src/main/java/eu/sadrian/quizshift/model/Achievements.java
@@ -0,0 +1,32 @@
+package eu.sadrian.quizshift.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "achievements")
+@Getter
+@Setter
+public class Achievements {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, length = 50)
+    private String code;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private int xp_reward = 0;
+
+    @Column
+    private int coins_reward = 0;
+}
