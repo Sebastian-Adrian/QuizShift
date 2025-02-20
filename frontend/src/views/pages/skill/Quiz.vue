c4ff2ffcfbfd8a2170a1ed8104a7f85ced1c5c2d
Sebastian@Thinkpad <mail@sadrian.eu>
2025-02-20 12:12:08 +0100

    Add quiz route, UI draft
    Introduce a new quiz route with a basic UI implementation in the frontend, including quiz type selection logic.

--- frontend/src/views/pages/skill/Quiz.vue
+++ frontend/src/views/pages/skill/Quiz.vue
@@ -1,13 +1,21 @@
-<script>
-export default {
-    name: "Quiz"
-}
+<script setup>
+import { ref } from 'vue';
+
+const popover = ref();
+const selectedQuizType = ref(null);
+const quizType = ref(['Multiple Choice', 'Single Choice']);
 </script>
 
 <template>
-
+    <Card>
+        <template #title>Quiz erstellen</template>/
+        <template #content>
+            <p class="m-10">Erstellen hier ein neues Quiz</p>
+        </template>
+        <div class="card flex justify-center">
+            <Button label="Quiztyp" icon="pi pi-chevron-circle-down" @click="popover.toggle"/>
+        </div>
+    </Card>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
